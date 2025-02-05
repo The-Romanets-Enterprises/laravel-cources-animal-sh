4e3941cf48a9190af1862bdbd5a01eb01b0273cf
VladZanevsky <zanevskij_va_21@mf.grsu.by>
2025-02-06 00:41:10 +0300

    update: unique rule field name in CountryRequest

--- app/Http/Requests/CountryRequest.php
+++ app/Http/Requests/CountryRequest.php
@@ -22,7 +22,7 @@ class CountryRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'max:255'],
+            'name' => ['required', 'max:255', 'unique:countries,name'],
         ];
     }
 }
