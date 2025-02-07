33ba65825521d8958d218dacfcee3f3469b29ffa
Baklajik <denisdenis20050315@gmail.com>
2025-02-08 00:24:02 +0300

    add: create, edit for Address

---
+++ resources/views/admin/address/fields.blade.php
@@ -0,0 +1,34 @@
+@include('layouts.form.text', [
+    'title' => 'Адрес*',
+    'name' => 'address',
+    'placeholder' => "Адрес",
+    'value' => $address->address ?? null,
+])
+
+@include('layouts.form.text', [
+    'title' => 'Почтовый индекс*',
+    'name' => 'post_index',
+    'placeholder' => "Почтовый индекс",
+    'value' => $address->post_index ?? null,
+])
+
+@include('layouts.form.select', [
+    'title' => 'Пользователь*',
+    'name' => 'user_id',
+    'items' => $users,
+    'value' => $address->user_id ?? null,
+    'key_value' => 'id',
+    'display_name' => 'name',
+    'pre_text' => 'Выберите пользователя'
+])
+
+@include('layouts.form.select', [
+    'title' => 'Город*',
+    'name' => 'city_id',
+    'items' => $cities,
+    'value' => $address->city_id ?? null,
+    'key_value' => 'id',
+    'display_name' => 'name',
+    'pre_text' => 'Выберите город'
+])
+
