dbe08fa3478b4d6d27e8ce1c87b1e9718699c195
Baklajik <denisdenis20050315@gmail.com>
2025-02-08 13:44:03 +0300

    add: AnimalPetRequest, PhotoRequest, VideoRequest

---
+++ app/Http/Requests/AnimalPetRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\Sex;
+use Illuminate\Foundation\Http\FormRequest;
+
+class AnimalPetRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'animal_id' => ['required', 'integer', 'exists:animals,id'],
+            'sex' => ['required', Rule::enum(Sex::class)],
+            'description' => ['nullable', 'string'],
+            'is_confirmed' => ['required', 'boolean'],
+            'user_id' => ['required', 'integer', 'exists:users,id'],
+            'birth_date' => ['required', 'date', 'before_or_equal:today'],
+            'is_sterilized' => ['required', 'boolean'],
+            'has_vaccination' => ['required', 'boolean'],
+            'wool_type' => ['nullable', 'string'],
+            'character' => ['nullable', 'string'],
+        ];
+    }
+}
