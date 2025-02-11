e9c9a520734c67b75f34892624873651304a0b2b
gr1_1nd <grund2007@mail.ru>
2025-02-11 22:37:11 +0300

    Created: City(view, Controller)

---
+++ resources/views/admin/city/fields.blade.php
@@ -0,0 +1,22 @@
+@php
+    use App\Enum\Sex;
+@endphp
+
+
+@include('layouts.form.text', [
+    'title' => 'Название города*',
+    'name' => 'name',
+    'placeholder' => "Название города",
+    'value' => $city->name ?? null,
+])
+
+@include('layouts.form.select', [
+    'title' => 'Страна*',
+    'name' => 'country_id',
+    'items' => $countries,
+    'value' => $city->country_id ?? null,
+    'key_value' => 'id',
+    'display_name' => 'name',
+    'pre_text' => 'Выберите страну'
+])
+
