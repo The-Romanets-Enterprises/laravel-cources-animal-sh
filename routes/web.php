dee04ad138faed19f963f28725c0a98e25e7cc81
Baklajik <denisdenis20050315@gmail.com>
2025-02-14 17:36:00 +0300

    up: messages, routes

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\CityController;
 use App\Http\Controllers\Admin\CountryController;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\VideoController;
 use Illuminate\Support\Facades\Route;
 
 //Route::get('/', function () {
@@ -31,6 +32,7 @@ Route::prefix('/admin')->name('admin.')->group(function () {
             Route::resource('/addresses', AddressController::class);
             Route::resource('/animals', AnimalController::class);
             Route::resource('/animal_pets', AnimalPetController::class);
+            Route::resource('/videos', VideoController::class);
         });
 
         Route::controller(UserController::class)->group(function () {
