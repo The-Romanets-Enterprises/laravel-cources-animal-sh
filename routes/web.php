08192c3d234e4e09d52a6ee42270967c73e06592
VladZanevsky <zanevskij_va_21@mf.grsu.by>
2025-03-01 15:31:26 +0300

    up: changed the routes for a single authorization and user registration

--- routes/web.php
+++ routes/web.php
@@ -12,39 +12,41 @@ use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\VideoController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+// Главная страница для всех
+Route::get('/', [AuthController::class, 'welcome'])->name('index');
 
+// Единые маршруты для авторизации и регистрации
+Route::middleware('guest')->controller(AuthController::class)->group(function () {
+    Route::get('/login', 'login')->name('login.show');
+    Route::post('/login', 'auth')->name('login');
+    Route::get('/register', 'register')->name('register.show');
+    Route::post('/register', 'signup')->name('signup');
+});
+
+Route::middleware('auth')->controller(AuthController::class)->group(function () {
+    Route::get('/logout', 'logout')->name('logout');
+});
+
+// Маршруты для загрузки файлов
 Route::post('/file/upload', [FileUploadController::class, 'upload'])->name('file.upload');
 Route::delete('/file/delete', [FileUploadController::class, 'delete'])->name('file.delete');
 
-Route::prefix('/admin')->name('admin.')->group(function () {
-    Route::middleware('guest')->controller(AuthController::class)->group(function () {
-        Route::get('/login', 'login')->name('login.show');
-        Route::post('/login', 'auth')->name('login.auth');
+// Админская панель
+Route::prefix('/admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::middleware('super-admin')->group(function () {
+        Route::get('/', [AuthController::class, 'index'])->name('home');
+        Route::resource('users', UserController::class);
+        Route::resource('countries', CountryController::class);
+        Route::resource('cities', CityController::class);
+        Route::resource('animals', AnimalController::class);
+        Route::resource('animal-pets', AnimalPetController::class);
+        Route::resource('addresses', AddressController::class);
+        Route::resource('photos', PhotoController::class);
+        Route::resource('videos', VideoController::class);
     });
-    Route::middleware('auth')->group(function () {
-        Route::controller(AuthController::class)->group(function () {
-            Route::get('/logout', 'logout')->name('logout');
-            Route::get('/', 'index')->name('home');
 
-        });
-
-        Route::middleware('super-admin')->group(function () {
-            Route::resource('/users', UserController::class);
-            Route::resource('/countries', CountryController::class);
-            Route::resource('/cities', CityController::class);
-            Route::resource('/animals', AnimalController::class);
-            Route::resource('/animal-pets', AnimalPetController::class);
-            Route::resource('/addresses', AddressController::class);
-            Route::resource('/photos', PhotoController::class);
-            Route::resource('/videos', VideoController::class);
-        });
-
-        Route::controller(UserController::class)->group(function () {
-            Route::get('/change-password', 'changePassword')->name('change-password');
-            Route::post('/change-password', 'passwordStore')->name('change-password.store');
-        });
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/change-password', 'changePassword')->name('change-password');
+        Route::post('/change-password', 'passwordStore')->name('change-password.store');
     });
 });
